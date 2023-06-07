a85c9b6d254bf83f386da796a166f46486492565
Malith Anjana <malithanjana97@gmail.com>
2023-06-07 22:39:51 +0530

    Filter genre by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,9 +15,14 @@ export interface Game {
   metacritic: number;
 } //data type   {id:1, name:'test', background_image:'www.image.com',parent_platforms:[{platform:{id, name:'pc', slug:'pc'}}]}
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
