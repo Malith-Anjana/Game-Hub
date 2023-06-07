8098c97f6b5a213cc8011f4da0d3fce28f3aa79e
Malith Anjana <malithanjana97@gmail.com>
2023-06-07 18:22:52 +0530

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game{
     id:number;
     name:string;
     background_image: string;
-    parent_platforms:[{platform: Platform}]
+    parent_platforms:[{platform: Platform}];
+    metacritic:number;
 }//data type   {id:1, name:'test', background_image:'www.image.com',parent_platforms:[{platform:{id, name:'pc', slug:'pc'}}]}
 
 interface FetchGamesResponse {
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://api.rawg.io/api',
+    baseURL:'https://api.rawg.io/xapi',
     params:{
         key:'e03c828b76994d2184193ef01d1e13b3'
     }
