89cd90df61fd2565c5fd5835cb8d415ab0726f97
Malith Anjana <malithanjana97@gmail.com>
2023-06-07 19:21:25 +0530

    Show Loading Skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,41 +3,49 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug:string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-export interface Game{
-    id:number;
-    name:string;
-    background_image: string;
-    parent_platforms:[{platform: Platform}];
-    metacritic:number;
-}//data type   {id:1, name:'test', background_image:'www.image.com',parent_platforms:[{platform:{id, name:'pc', slug:'pc'}}]}
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: [{ platform: Platform }];
+  metacritic: number;
+} //data type   {id:1, name:'test', background_image:'www.image.com',parent_platforms:[{platform:{id, name:'pc', slug:'pc'}}]}
 
 interface FetchGamesResponse {
-    count:number;
-    results:Game[]
+  count: number;
+  results: Game[];
 }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
 
-     apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-     .then(res=> setGames(res.data.results))
-     .catch(err => {
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message);
+        setIsLoading(false);
+      });
 
-     return ()=> controller.abort();
-    }, [])
+    return () => controller.abort();
+  }, []);
 
-    return {games,error}
-}
+  return { games, error, isLoading };
+};
 
-export default useGames
+export default useGames;
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,6 @@
 //crop and reduce the image sizes to ge optimize
 const getCroppedImageUrl = (url: string)=> {
-    const target = 'media';
+    const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
